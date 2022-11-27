4773e91fe4cefa8505b4d2ef5916822eb3a56bd7
Brian Pruitt <brianrpruitt63@gmail.com>
2022-11-26 21:16:46 -0500

    tests for season build function and component

--- src/__tests__/NewSeason.test.js
+++ src/__tests__/NewSeason.test.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import NewSeason from '../NewSeason';
 
-const component = <BrowserRouter>
-                        <NewSeason />
-                    </BrowserRouter>
+
+const user = userEvent.setup();
 
 const renderComponent = (orgId) => {
     return render(<BrowserRouter>
                     <NewSeason orgId={orgId} />
-                </BrowserRouter>)
-}
+                </BrowserRouter>);
+};
+
+
+/** ACTIONS */
+const buttonClick = async (buttonText) => {
+    const button = screen.getByText(buttonText);
+    await user.click(button);
+};
+
+const formInput = async (identifier, message) => {
+    const input = identifier.placeholder ? 
+                    screen.getByPlaceholderText(identifier.placeholder)
+                    : screen.getByLabelText(identifier.label);
+    await user.click(input);
+    await user.keyboard(message);
+};
+
+const bulletClick = async (labelText) => {
+    const bullet = screen.getByLabelText(labelText);
+    await user.click(bullet);
+};
+
+
 
 test('renders from home', () => {
     renderComponent();
@@ -30,4 +52,81 @@ test('renders from org', () => {
 test('matches snapshot', () => {
     const {asFragment} = renderComponent();;
     expect(asFragment()).toMatchSnapshot();
+});
+
+test('requires season name anon', async () => {
+    renderComponent();
+    await buttonClick('Next');
+    const error = screen.getByText('Season title must be between 2 and 100 characters');
+    expect(error).toBeInTheDocument();
+});
+
+test('requires season name from org', async () => {
+    renderComponent(1);
+    await buttonClick('Next');
+    const error = screen.getByText('Season title must be between 2 and 100 characters');
+    expect(error).toBeInTheDocument();
+});
+
+test('name input works', async () => {
+    renderComponent();
+    await formInput({placeholder: 'Season Title'}, 'Test Season');
+    await buttonClick('Next');
+    expect(screen.getByLabelText('Number of Teams')).toBeInTheDocument();
+});
+
+test('anon keeps subnav', () => {
+    renderComponent();
+    expect(screen.queryByText('Cancel Season Create')).not.toBeInTheDocument();
+});
+
+test('no subnav from org', () => {
+    renderComponent(1);
+    expect(screen.getByText('Cancel Season Create')).toBeInTheDocument();
+});
+
+test('can skip team naming', async () => {
+    renderComponent();
+    await formInput({placeholder: 'Season Title'}, 'Test Season');
+    await buttonClick('Next');
+    await formInput({label: 'Number of Teams'}, '30');
+    await buttonClick('Next');
+    expect(screen.getByLabelText('Number of Weeks/Rounds')).toBeInTheDocument();
+    expect(screen.queryByText('Enter Team Names')).not.toBeInTheDocument();
+});
+
+test('no manual game entry anon', async () => {
+    renderComponent();
+    await formInput({placeholder: 'Season Title'}, 'Test Season');
+    await buttonClick('Next');
+    await formInput({label: 'Number of Teams'}, '30');
+    await buttonClick('Next');
+    expect(screen.getByLabelText('Number of Weeks/Rounds')).toBeInTheDocument();
+    expect(screen.queryByLabelText('Manually Enter Season Info')).not.toBeInTheDocument();
+});
+
+test('can manually enter games for an organization', async () => {
+    renderComponent(1);
+    await formInput({placeholder: 'Season Title'}, 'Test Season');
+    await buttonClick('Next');
+    await formInput({label: 'Number of Teams'}, '30');
+    await buttonClick('Next');
+    expect(screen.getByLabelText('Number of Weeks/Rounds')).toBeInTheDocument();
+    expect(screen.getByLabelText('Manually Enter Season Info')).toBeInTheDocument();
+});
+
+test('allows team naming', async () => {
+    renderComponent();
+    await formInput({placeholder: 'Season Title'}, 'Test Season');
+    await buttonClick('Next');
+    await formInput({label: 'Number of Teams'}, '30');
+    await bulletClick('Name Teams');
+    await buttonClick('Next');
+    const teams = screen.getAllByLabelText('Team Name');
+    expect(teams.length).toEqual(30);
+    expect(screen.getByText('Enter Team Names')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Team 1')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Team 23')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Team 30')).toBeInTheDocument();
+    expect(screen.queryByPlaceholderText('Team 31')).not.toBeInTheDocument();
 });
---
+++ src/__tests__/seasonBuild.test.js
@@ -0,0 +1,122 @@
+import {buildSeason} from '../static/helpers/seasonBuild';
+
+const buildTeamsArray = (size) => {
+    const teams = [];
+    for (let num = 1; num <= size; num++){
+        teams.push({teamName: `Team ${num}`, color: 'N/A'});
+    };
+    return teams;
+};
+
+const evenRoundRobin = (games) => {
+    const matchupCount = {};
+    for (let game of games) {
+        const {team1Name, team2Name} = game;
+        if (team1Name !== 'Bye' && team2Name !== 'Bye') {
+            const matchup = [team1Name, team2Name].sort().join('');
+            matchupCount[matchup] = matchupCount[matchup] + 1 || 1;
+        };
+    };
+    const vals = Object.values(matchupCount);
+    vals.sort((a, b) => a - b);
+    return vals[vals.length - 1] - vals[0] <= 1 ? true : false;
+};
+
+const evenHomeAway = (games) => {
+    const locationCount = {};
+    for (let game of games) {
+        const {team1Name, team2Name} = game;
+        if (team1Name !== 'Bye' && team2Name !== 'Bye') {
+            team1Name in locationCount ? locationCount[team1Name][0]++ 
+                                        : locationCount[team1Name] = [1, 0];
+            team2Name in locationCount ? locationCount[team2Name][1]++ 
+                                        : locationCount[team2Name] = [0, 1];
+        };
+    };
+    const vals = Object.values(locationCount);
+    for (let val of vals) {
+        if (Math.abs(val[0] - val[1]) > 1) {console.log(vals); console.log(games.length); return false;}
+    };
+    return true;
+};
+
+
+test('works', () => {
+    const {teams, games} = buildSeason(buildTeamsArray(2), 1);
+    expect(teams.length).toEqual(3);
+    expect(games.length).toEqual(1);
+});
+
+test('no byes if even number of teams', () => {
+    let games = buildSeason(buildTeamsArray(4), 6).games;
+    expect(games.length).toEqual(12);
+
+    games = buildSeason(buildTeamsArray(8), 6).games;
+    expect(games.length).toEqual(24);
+
+    games = buildSeason(buildTeamsArray(2), 20).games;
+    expect(games.length).toEqual(20);
+
+    games = buildSeason(buildTeamsArray(30), 3).games;
+    expect(games.length).toEqual(45);
+});
+
+test('byes if odd number of teams', () => {
+    let games = buildSeason(buildTeamsArray(5), 6).games;
+    expect(games.length).not.toEqual(15);
+
+    games = buildSeason(buildTeamsArray(9), 6).games;
+    expect(games.length).not.toEqual(27);
+
+    games = buildSeason(buildTeamsArray(3), 20).games;
+    expect(games.length).not.toEqual(30);
+
+    games = buildSeason(buildTeamsArray(29), 4).games;
+    expect(games.length).not.toEqual(58);
+});
+
+test('each team plays each team about the same number of times', () => {
+    let games = buildSeason(buildTeamsArray(4), 6).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(8), 6).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(5), 4).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(29), 10).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(30), 10).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(29), 11).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(30), 11).games;
+    expect(evenRoundRobin(games)).toBeTruthy();
+});
+
+test('each team plays about the same amount of home/away games', () => {
+    let games = buildSeason(buildTeamsArray(4), 6).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(8), 6).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(5), 4).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(29), 10).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(30), 10).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(29), 11).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+
+    games = buildSeason(buildTeamsArray(30), 11).games;
+    expect(evenHomeAway(games)).toBeTruthy();
+});
